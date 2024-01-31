8632efeed5c9a87e9694c4b71753f01de973c980
taro-28 <tarotaro0208@gmail.com>
2024-02-01 08:52:39 +0900

    hoge

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,43 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useFragment, useQuery } from "@apollo/client";
 import "./App.css";
 
+const LOCATION_FIELDS = gql`
+  fragment Loc on Location {
+    name
+    description
+  }
+`;
+
 const GET_LOCATIONS = gql`
+  ${LOCATION_FIELDS}
   query GetLocations {
     locations {
       id
-      name
-      description
-      photo
+      ...Loc
     }
   }
 `;
+
+const Location = ({ from }: { from: string }) => {
+  const {
+    complete,
+    data: { name, description },
+  } = useFragment({
+    fragment: LOCATION_FIELDS,
+    fragmentName: "Loc",
+    from,
+  });
+  return (
+    <div>
+      <h3>{name}</h3>
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
+      <p>complete:{JSON.stringify(complete)}</p>
+    </div>
+  );
+};
 
 function App() {
   const { loading, error, data } = useQuery(GET_LOCATIONS);
@@ -18,15 +45,7 @@ function App() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
 
-  return data.locations.map(({ id, name, description }) => (
-    <div key={id}>
-      <h3>{name}</h3>
-      <br />
-      <b>About this location:</b>
-      <p>{description}</p>
-      <br />
-    </div>
-  ));
+  return data.locations.map(({ id }) => <Location key={id} from={id} />);
 }
 
 export default App;
